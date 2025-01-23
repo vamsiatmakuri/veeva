e01c91688639103ec12e24ca7d03dd097f856d93
vamsiatmakuri <atmakurivamsikrishna@gmail.com>
2025-01-23 11:21:53 +0530

    Update SequentialRunner.java

--- src/test/java/runner/SequentialRunner.java
+++ src/test/java/runner/SequentialRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/java/features/SampleTest.feature",
-        glue = "com/sneo/bdd/stepdefinitions",
+        glue = "com/bdd/stepdefinitions",
         plugin = { "pretty", "html:target/cucumber-reports/cucumber.html","json:target/cucumber-reports/Cucumber.json","junit:target/cucumber-reports/Cucumber.xml" },
         monochrome = false
 )
